9c42c7e87bc17eded8a5b96b5996e3dc1da629c1
anubhav7790 <anubhavkumar779@gmail.com>
2025-09-26 20:02:50 +0530

    Maximum product of three Numbers

---
+++ Q628.java
@@ -0,0 +1,11 @@
+// package JAVA-DSA--DPP;
+import java.util.Arrays;
+public class Q628 {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int max1 = nums[n-3]*nums[n-2]*nums[n-1];
+        int max2 = nums[0]*nums[1]*nums[n-1];
+        return Math.max(max1 , max2) ;
+    }
+}
