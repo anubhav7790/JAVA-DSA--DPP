9bf929e2c9cf89a388cbde64d0c9ca02f1f262c3
anubhav7790 <anubhavkumar779@gmail.com>
2025-09-26 19:54:09 +0530

    Best Time to Buy ans Sell Stock

---
+++ Q121.java
@@ -0,0 +1,19 @@
+package JAVA-DSA--DPP;
+
+public class Q121 {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE ;
+        int maxprofit = 0; 
+        for(int price : prices){
+            if(price < min ){
+                min = price ; 
+            }
+
+            else if(price - min > maxprofit){
+                maxprofit = price-min ; 
+            }
+        }
+        return maxprofit ; 
+    }
+
+}
